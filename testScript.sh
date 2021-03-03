aa8b32fbe30a4f48e0db9ad3cf12bf5209939023
Sarah <pappas.sarah@gmail.com>
2021-03-03 10:22:16 -0800

    clean up curl command

--- testScript.sh
+++ testScript.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# ENCODED_IMG=@./imgFileEncoded
-# ENCODED_IMG=$(base64 ~/Downloads/malith-d-karunarathne-qIRJeKdieKA-unsplash.jpg)
-
 curl --header "Content-Type: application/json"\
     --request POST\
     --data '{"img": "'$(base64 ~/Downloads/super-small-test.png)'"}'\
     http://localhost:3000/processImg
-
-#curl --request POST --header "Content-Type: application/json" --data '{"img": "test"}' http://localhost:3000/processImg
-# curl --request POST --header "Content-Type: application/json" --data '{"img": "test"}' http://localhost:3000/processImg
-
-#-H "Accept: application/json"
-    # --data '{"img": "'"@./imgFileEncoded.txt"'"}'\
