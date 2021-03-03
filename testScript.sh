7c51bf11f88063fc8468aba590e1c47cdcd883c1
Sarah <pappas.sarah@gmail.com>
2021-03-02 22:04:57 -0800

    working on curl command

--- testScript.sh
+++ testScript.sh
@@ -3,9 +3,9 @@
 # ENCODED_IMG=@./imgFileEncoded
 # ENCODED_IMG=$(base64 ~/Downloads/malith-d-karunarathne-qIRJeKdieKA-unsplash.jpg)
 
-curl --header "Content-Type: application/text"\
+curl --header "Content-Type: application/json"\
     --request POST\
-    --data '{"img": "'$(base64 ~/Downloads/super-small-test.png)'"}'\
+    --data '{"img": "iVB"}'\
     http://localhost:3000/processImg
 
 #curl --request POST --header "Content-Type: application/json" --data '{"img": "test"}' http://localhost:3000/processImg
