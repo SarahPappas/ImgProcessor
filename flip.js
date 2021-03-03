e9e20e56d3f638890f22b1a66da53994fbd0e7b8
Sarah <pappas.sarah@gmail.com>
2021-03-03 12:34:33 -0800

    write image after flip

--- flip.js
+++ flip.js
@@ -5,7 +5,11 @@ function flip(filePath) {
         throw "flip is missing file path";
     }
 
-    gm(filePath).flip();
+    gm(filePath).flip()
+    .write(filePath, function (err) {
+        if(err) console.log(err);
+        console.log("flipped!")
+    });;
 }
 
 module.exports = flip;
